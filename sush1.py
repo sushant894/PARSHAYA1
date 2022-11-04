4f9327d0b4d01c369572fcdfd209cae60768e77d
sushant Manoj singh <sushantmanojsingh894@gmail.com>
2022-11-04 23:54:29 +0530

    initial commit

---
+++ sush1.py
@@ -0,0 +1,8 @@
+adj=input("Enter the adj u want:")
+verb = input("Enter the most exciting verb u want:")
+verb2 = input("Enter the next exciting verb u want: ")
+noun= input("name the cartoon character u want:")
+
+madlib=(f"My name is sushant and i am {adj}. i have more quality than u deserve {verb}  \
+kon bola kon bola {verb2}.My favourite hero is {noun}")
+print(madlib)
